740cc7837a26cada00efaec97756ab62ed636a83
Kilian Ruchti <kilian.ruchti@gmail.com>
2023-02-03 23:00:40 +0100

    Floor and Walls Generator

--- Assets/Kilian/RoomGenerator.cs
+++ Assets/Kilian/RoomGenerator.cs
@@ -7,9 +7,11 @@ public class RoomGenerator : MonoBehaviour
 {
     public int xRoom = 10;
     public int zRoom = 5;
-    public GameObject floorTile;
-    public Transform floorParent;
+    public GameObject floorPrefab;
+    public GameObject wallPrefab;
+    public Transform roomParent;
     private ArrayList floorArr = new ArrayList();
+    private ArrayList wallArr = new ArrayList();
     private (int tileID, Vector3 tilePosition) tileObj;
 
     // Start is called before the first frame update
@@ -24,6 +26,7 @@ public class RoomGenerator : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Return))
         {
             GenerateFloor(xRoom, zRoom);
+            GenerateWalls(xRoom, zRoom);
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -32,11 +35,9 @@ public class RoomGenerator : MonoBehaviour
         }
     }
 
-   
-
     private void GenerateFloor(int xRoom, int zRoom)
     {
-        Debug.Log("GenerateFloor");
+        Debug.Log("Generate the Floor");
 
         for (int x = 0; x < xRoom; x++)
         {
@@ -46,9 +47,31 @@ public class RoomGenerator : MonoBehaviour
                 Vector3 tilePosition = new Vector3(x, 0, z);
                 tileObj = (tileID, tilePosition);
 
-                floorArr.Add(Instantiate(floorTile, tileObj.tilePosition, Quaternion.identity, floorParent));
+                floorArr.Add(Instantiate(floorPrefab, tileObj.tilePosition, Quaternion.identity, roomParent));
             }
         }        
+    }
+
+    private void GenerateWalls(int xRoom, int zRoom)
+    {
+        Debug.Log("Generate Walls");
+        for (int x = 0; x < xRoom; x++)
+        {
+            int wallID = 1;
+            Vector3 wallPosition = new Vector3(x, 0, zRoom);
+            tileObj = (wallID, wallPosition);
+
+            wallArr.Add(Instantiate(wallPrefab, tileObj.tilePosition, Quaternion.identity, roomParent));
+        }
+
+        for (int z = 0; z < zRoom; z++)
+        {
+            int wallID = 1;
+            Vector3 wallPosition = new Vector3(xRoom, 0, z);
+            tileObj = (wallID, wallPosition);
+
+            wallArr.Add(Instantiate(wallPrefab, tileObj.tilePosition, Quaternion.identity, roomParent));
+        }
     }
 
     private void ClearTiles()
