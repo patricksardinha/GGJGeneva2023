8397dfb8e923afa9e04637ad532dff5c05499b5d
BenjaminGonvers <gonversbenjamin@gmail.com>
2023-02-04 10:50:57 +0100

    Creation du script des ennemy et de leur IA, ajouts du playerscript

---
+++ Assets/Benjamin/EnnemyScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyScript : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 20f;
+    [SerializeField] private float attackRange = 1f;
+    [SerializeField] private float attackSpeed = 1f;
+    [SerializeField] private float attackDamage = 1f;
+    [SerializeField] private float life = 1f;
+    [SerializeField] private GameObject IaBehavior;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+       
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player touched");
+            collision.gameObject.GetComponent<PlayerScript>().TakeDamage(attackDamage);
+            //need to use attack animation
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        life -= damage;
+        Debug.Log("Ennemy life: " + life);
+        //need to use taking damage animation
+        //and if life <= 0, need to use death animation
+    }
+
+
+}
