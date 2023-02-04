5729d9eac648b03062d7e58450f808a821ee4576
patricksardinha <101988616+patricksardinha@users.noreply.github.com>
2023-02-04 15:57:33 +0100

    modif anim player

--- Assets/0_Main/Script/MovingScript.cs
+++ Assets/0_Main/Script/MovingScript.cs
@@ -5,6 +5,8 @@ using UnityEngine.InputSystem;
 
 public class MovingScript : MonoBehaviour
 {
+    private Animator playerAnimator;
+
     //speed in pixels per second
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float speed = 50f;
@@ -14,7 +16,7 @@ public class MovingScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,13 +27,31 @@ public class MovingScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector3(PlayerMovement.x, 0, PlayerMovement.y) * speed * Time.deltaTime;
-        //transform.Translate(new Vector3(PlayerMovement.x * speed, 0, PlayerMovement.y * speed) * Time.deltaTime);
+        if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("AttackRange") || playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("AttackMelee"))
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+        } 
+        else
+        {
+            rb.velocity = new Vector3(PlayerMovement.x, 0, PlayerMovement.y) * speed * Time.deltaTime;
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         Vector2 input = context.ReadValue<Vector2>();
+        
+        if (input.x != 0 || input.y != 0)
+        {
+            playerAnimator.SetFloat("playerSpeed", 1);
+            playerAnimator.SetBool("isRunning", true);
+        } 
+        else
+        {
+            playerAnimator.SetFloat("playerSpeed", 0);
+            playerAnimator.SetBool("isRunning", false);
+        }
+
         PlayerMovement = Quaternion.Euler(0, 0, -45) * input;
 
     }
